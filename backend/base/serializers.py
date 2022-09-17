e688665a6ec4f7ea6f6c27d5d055576de11b0290
Khushiyant <khushiyant2002@gmail.com>
2022-09-18 00:28:26 +0530

    serializers : created NGO, Community, User model serializers

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -1,1 +1,17 @@
 from rest_framework import serializers
+from .models import NGO, Community, UserAccount
+
+class UserAccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserAccount
+        fields = '__all__'
+
+class NGOSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NGO
+        fields = '__all__'
+
+class CommunitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Community
+        fields = '__all__'
