8f8b42385391974a5f88cc94e83b3ea62369d1c9
Khushiyant <khushiyant2002@gmail.com>
2022-09-17 23:55:05 +0530

    base.api : remove api app clustering

--- backend/base/models.py
+++ backend/base/models.py
@@ -1,3 +1,33 @@
+import email
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class UserAccount(AbstractUser):
+    pass
+
+class NGO(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    user = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
+    location = models.CharField(max_length=100)
+    staff_count = models.IntegerField()
+    volunteers_count = models.IntegerField()
+    coordinator = models.ForeignKey(UserAccount, on_delete=models.CASCADE, related_name="coordinator")
+    email = models.EmailField()
+    phone = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+class Community(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    user = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
+    location = models.CharField(max_length=100)
+    members_count = models.IntegerField()
+    coordinator = models.ForeignKey(UserAccount, on_delete=models.CASCADE, related_name="coordinator")
+    email = models.EmailField()
+    phone = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
