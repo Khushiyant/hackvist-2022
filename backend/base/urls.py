385b7d02a2a454177b67c5e7effdb26f74925428
Khushiyant <khushiyant2002@gmail.com>
2022-09-18 10:55:50 +0530

    auth : add login, register api

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("api/", views.apiOverview, name="api-overview"),
+    path('login/', views.login),
+    path('registration/', views.register),
 ]
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,10 +1,53 @@
 import json
 
+from django.contrib.auth import authenticate
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from .models import NGO, Community, UserAccount
+from .serializers import (CommunitySerializer, NGOSerializer,
+                          UserAccountSerializer, UserLoginSerializer,
+                          UserRegistrationSerializer)
+
+
+def get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user)
+
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
+
 
 @api_view(['GET'])
 def apiOverview(request):
     overview = json.load(open('json/api.json'))
     return Response(overview, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+def register(request):
+    data = request.data
+    serializer = UserRegistrationSerializer(data=data)
+    serializer.is_valid(raise_exception=True)
+    user = serializer.save()
+    token = get_tokens_for_user(user)
+
+    return Response({'token': token, 'message': 'Registration Successful'}, status=status.HTTP_201_CREATED)
+
+
+@api_view(['POST'])
+def login(request):
+    serializer = UserLoginSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    email = serializer.validated_data['email']
+    password = serializer.validated_data['password']
+    user = authenticate(email=email, password=password)
+    if user is not None:
+        token = get_tokens_for_user(user)
+        return Response({'token': token, 'msg': 'Login Success'}, status=status.HTTP_200_OK)
+    else:
+        return Response({'errors': {'non_field_errors': ['Email or Password is not Valid']}}, status=status.HTTP_404_NOT_FOUND)
