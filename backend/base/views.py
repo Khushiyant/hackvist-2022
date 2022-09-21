d44e4a1f0b188d2b14d1d12f6abe586f6edb6fae
Khushiyant <khushiyant2002@gmail.com>
2022-09-21 18:50:06 +0530

    api endpoints : update, register

--- backend/base/views.py
+++ backend/base/views.py
@@ -60,3 +60,67 @@ def login(request):
         return Response({'token': token, 'msg': 'Login Success'}, status=status.HTTP_200_OK)
     else:
         return Response({'errors': {'non_field_errors': ['Email or Password is not Valid']}}, status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def ngo_register(request):
+    # -------Under development--------
+    data = request.data
+    serializer = NGOSerializer(data=data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response({'message': 'NGO Registration Successful'}, status=status.HTTP_201_CREATED)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def community_register(request):
+    # -------Under development--------
+    data = request.data
+    serializer = CommunitySerializer(data=data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response({'message': 'Community Registration Successful'}, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def user_details(request):
+    user = request.user
+    serializer = UserAccountSerializer(user)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def update_ngo(request):
+    # -------Under development--------
+    user = request.user
+    ngo = NGO.objects.get(user=user)
+    serializer = NGOSerializer(instance=ngo, data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response({'message': 'NGO Details Updated'}, status=status.HTTP_200_OK)
+
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def update_community(request):
+    # -------Under development--------
+    user = request.user
+    community = Community.objects.get(user=user)
+    serializer = CommunitySerializer(instance=community, data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response({'message': 'Community Details Updated'}, status=status.HTTP_200_OK)
+
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def update_user(request):
+    user = request.user
+    serializer = UserAccountSerializer(instance=user, data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response({'message': 'User Details Updated'}, status=status.HTTP_200_OK)
